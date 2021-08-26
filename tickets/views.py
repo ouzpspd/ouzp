44d4142f2d8752a671dab37a04e4437e443e0a2e
30padavan10 <pankov.dmitriy@itmh.ru>
2021-08-26 13:55:44 +0500

    test3

--- tickets/views.py
+++ tickets/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.http import Http404
 
-print('!!!')
+print('!!!!!!')
 
 
 def register(request):
