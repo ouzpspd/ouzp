36bfefa479ac71c10102b8c672bb049202b1bcec
30padavan10 <pankov.dmitriy@itmh.ru>
2022-03-24 18:05:27 +0500

    fix 128, 131 vlan

--- tickets/views.py
+++ tickets/views.py
@@ -521,7 +521,7 @@ def copper(request):
                         type_passage = request.session['type_passage']
                         if type_passage == 'Перенос сервиса в новую точку' or (type_passage == 'Перевод на гигабит' and not any([logic_change_csw, logic_change_gi_csw])):
                             selected_service = selected_ono[0][-3]
-                            service_shpd = ['DA', 'BB', 'ine', 'Ine', '128 -', '53 -', '34 -', '33 -', '32 -', '54 -', '57 -', '60 -', '62 -', '64 -', '68 -', '92 -', '96 -', '101 -', '105 -', '125 -', '107 -', '109 -', '483 -']
+                            service_shpd = ['DA', 'BB', 'ine', 'Ine', '128 -', '53 -', '34 -', '33 -', '32 -', '54 -', '57 -', '60 -', '62 -', '64 -', '68 -', '92 -', '96 -', '101 -', '105 -', '125 -', '131 -', '128 -', '107 -', '109 -', '483 -']
                             if any(serv in selected_service for serv in service_shpd):
                                 tag_service.append({'change_log_shpd': None})
                                 request.session['subnet_for_change_log_shpd'] = selected_ono[0][-4]
@@ -653,7 +653,7 @@ def vols(request):
                             selected_service = selected_ono[0][-3]
                             service_shpd = ['DA', 'BB', 'ine', 'Ine', '128 -', '53 -', '34 -', '33 -', '32 -', '54 -',
                                             '57 -', '60 -', '62 -', '64 -', '68 -', '92 -', '96 -', '101 -', '105 -',
-                                            '125 -', '107 -', '109 -', '483 -']
+                                            '125 -', '131 -', '128 -', '107 -', '109 -', '483 -']
                             if any(serv in selected_service for serv in service_shpd):
                                 tag_service.append({'change_log_shpd': None})
                                 request.session['subnet_for_change_log_shpd'] = selected_ono[0][-4]
@@ -819,7 +819,7 @@ def wireless(request):
                             selected_service = selected_ono[0][-3]
                             service_shpd = ['DA', 'BB', 'ine', 'Ine', '128 -', '53 -', '34 -', '33 -', '32 -', '54 -',
                                             '57 -', '60 -', '62 -', '64 -', '68 -', '92 -', '96 -', '101 -', '105 -',
-                                            '125 -', '107 -', '109 -', '483 -']
+                                            '125 -', '131 -', '128 -', '107 -', '109 -', '483 -']
                             if any(serv in selected_service for serv in service_shpd):
                                 tag_service.append({'change_log_shpd': None})
                                 request.session['subnet_for_change_log_shpd'] = selected_ono[0][-4]
@@ -2603,7 +2603,7 @@ def head(request):
         request.session['node_csw'] = node_csw
 
     service_shpd = ['DA', 'BB', 'ine', 'Ine', '128 -', '53 -', '34 -', '33 -', '32 -', '54 -', '57 -', '60 -', '62 -',
-                    '64 -', '68 -', '92 -', '96 -', '101 -', '105 -', '125 -', '107 -', '109 -', '483 -']
+                    '64 -', '68 -', '92 -', '96 -', '101 -', '105 -', '125 -', '131 -', '128 -', '107 -', '109 -', '483 -']
     service_shpd_bgp = ['BGP', 'bgp']
     service_portvk = ['-vk', 'vk-', '- vk', 'vk -', 'zhkh']
     service_portvm = ['-vrf', 'vrf-', '- vrf', 'vrf -']
