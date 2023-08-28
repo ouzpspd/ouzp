24096c94c6138082e658ff92fc0aa341e79997ec
ouzpspd <30padavan10@gmail.com>
2023-08-28 12:35:06 +0500

    COMMENT. Service form commented

--- oattr/views.py
+++ oattr/views.py
@@ -260,79 +260,84 @@ def construct_tr(value_vars, service_vars, templates, ticket_tr):
         hidden_vars['Согласование:'] = 'Согласование:'
         hidden_vars['%Согласование%'] = '%Согласование%'
         static_vars['Согласование'] = value_vars.get('agreement')
+    if value_vars.get('equipment'):
+        hidden_vars['Монтаж оборудования:'] = 'Монтаж оборудования:'
+        hidden_vars['%Монтаж оборудования%'] = '%Монтаж оборудования%'
+        static_vars['Монтаж оборудования'] = value_vars.get('equipment')
     static_vars['Доступ'] = value_vars.get('access')
+
     result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
 
-    services = add_tag_for_services(ticket_tr)
-    repr_string['mounting_line_service'] = '- Смонтировать %Количество линий связи% линии %Тип кабеля% от %Точка от% до %Точка до%. %Способ монтажа линии связи%. %Способ крепежа линии связи%.'
-    for tag_service in services.keys():
-        line_exist = bool([True for key in service_vars.keys() if key.startswith(f'{tag_service}_from_')])
-
-        if tag_service.startswith('lvs') and line_exist:
-            if service_vars.get('lvs_switch'):
-                template = templates.get('Организация ЛВС')
-            else:
-                template = templates.get('Организация СКС')
-            static_vars = {}
-            hidden_vars = {}
-            #repr_string = {}
-
-            multi_vars = {repr_string['mounting_line_service']: []}
-            count_lines = [key.strip('lvs_from_') for key in service_vars.keys() if key.startswith('lvs_from_')]
-            for i in count_lines:
-                static_vars[f'Количество линий связи {i}'] = service_vars.get(f'lvs_count_line_{i}')
-                static_vars[f'Тип кабеля {i}'] = service_vars.get(f'lvs_cable_{i}')
-                static_vars[f'Точка от {i}'] = service_vars.get(f'lvs_from_{i}')
-                static_vars[f'Точка до {i}'] = service_vars.get(f'lvs_to_{i}')
-                static_vars[f'Способ монтажа линии связи {i}'] = service_vars.get(f'lvs_mounting_{i}')
-                static_vars[f'Способ крепежа линии связи {i}'] = service_vars.get(f'lvs_fastening_{i}')
-                multi_vars[repr_string['mounting_line_service']].append(f'- Смонтировать %Количество линий связи {i}% линии %Тип кабеля {i}% от %Точка от {i}%' +
-                                                                f' до %Точка до {i}%. %Способ монтажа линии связи {i}%. %Способ крепежа линии связи {i}%.')
-            result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
-
-        elif tag_service.startswith('phone'):
-            template = templates.get('Организация Телефонии')
-            static_vars = {}
-            hidden_vars = {}
-            #repr_string = {}
-            multi_vars = {repr_string['mounting_line_service']: []}
-            count_lines = [key.strip('phone_from_') for key in service_vars.keys() if key.startswith('phone_from_')]
-            for i in count_lines:
-                static_vars[f'Количество линий связи {i}'] = service_vars.get(f'phone_count_line_{i}')
-                static_vars[f'Тип кабеля {i}'] = service_vars.get(f'phone_cable_{i}')
-                static_vars[f'Точка от {i}'] = service_vars.get(f'phone_from_{i}')
-                static_vars[f'Точка до {i}'] = service_vars.get(f'phone_to_{i}')
-                static_vars[f'Способ монтажа линии связи {i}'] = service_vars.get(f'phone_mounting_{i}')
-                static_vars[f'Способ крепежа линии связи {i}'] = service_vars.get(f'phone_fastening_{i}')
-                multi_vars[repr_string['mounting_line_service']].append(f'- Смонтировать %Количество линий связи {i}% линии %Тип кабеля {i}% от %Точка от {i}%' +
-                                                                f' до %Точка до {i}%. %Способ монтажа линии связи {i}%. %Способ крепежа линии связи {i}%.')
-            if service_vars.get('phone_vgw_place') != 'не требуется':
-                hidden_vars['Установка оборудования:'] = 'Установка оборудования:'
-                hidden_vars['- Установить тел. шлюз %Место голос. шлюза%.'] = '- Установить тел. шлюз %Место голос. шлюза%.'
-                static_vars['Место голос. шлюза'] = service_vars.get('phone_vgw_place')
-            result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
-
-        elif tag_service.startswith('video'):
-            template = templates.get('Организация СВН')
-            static_vars = {}
-            hidden_vars = {}
-            #repr_string = {}
-            multi_vars = {repr_string['mounting_line_service']: []}
-            count_lines = [key.strip('video_from_') for key in service_vars.keys() if key.startswith('video_from_')]
-            for i in count_lines:
-                static_vars[f'Количество линий связи {i}'] = service_vars.get(f'video_count_line_{i}')
-                static_vars[f'Тип кабеля {i}'] = service_vars.get(f'video_cable_{i}')
-                static_vars[f'Точка от {i}'] = service_vars.get(f'video_from_{i}')
-                static_vars[f'Точка до {i}'] = service_vars.get(f'video_to_{i}')
-                static_vars[f'Способ монтажа линии связи {i}'] = service_vars.get(f'video_mounting_{i}')
-                static_vars[f'Способ крепежа линии связи {i}'] = service_vars.get(f'video_fastening_{i}')
-                multi_vars[repr_string['mounting_line_service']].append(f'- Смонтировать %Количество линий связи {i}% линии %Тип кабеля {i}% от %Точка от {i}%' +
-                                                                f' до %Точка до {i}%. %Способ монтажа линии связи {i}%. %Способ крепежа линии связи {i}%.')
-            if service_vars.get('video_switch'):
-                hidden_vars['- По согласованию с клиентом установить POE-коммутатор в помещении клиента.'] = \
-                    '- По согласованию с клиентом установить POE-коммутатор в помещении клиента.'
-            static_vars['Количество камер'] = service_vars.get('video_count_camera')
-            result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
+    # services = add_tag_for_services(ticket_tr)
+    # repr_string['mounting_line_service'] = '- Смонтировать %Количество линий связи% линии %Тип кабеля% от %Точка от% до %Точка до%. %Способ монтажа линии связи%. %Способ крепежа линии связи%.'
+    # for tag_service in services.keys():
+    #     line_exist = bool([True for key in service_vars.keys() if key.startswith(f'{tag_service}_from_')])
+    #
+    #     if tag_service.startswith('lvs') and line_exist:
+    #         if service_vars.get('lvs_switch'):
+    #             template = templates.get('Организация ЛВС')
+    #         else:
+    #             template = templates.get('Организация СКС')
+    #         static_vars = {}
+    #         hidden_vars = {}
+    #         #repr_string = {}
+    #
+    #         multi_vars = {repr_string['mounting_line_service']: []}
+    #         count_lines = [key.strip('lvs_from_') for key in service_vars.keys() if key.startswith('lvs_from_')]
+    #         for i in count_lines:
+    #             static_vars[f'Количество линий связи {i}'] = service_vars.get(f'lvs_count_line_{i}')
+    #             static_vars[f'Тип кабеля {i}'] = service_vars.get(f'lvs_cable_{i}')
+    #             static_vars[f'Точка от {i}'] = service_vars.get(f'lvs_from_{i}')
+    #             static_vars[f'Точка до {i}'] = service_vars.get(f'lvs_to_{i}')
+    #             static_vars[f'Способ монтажа линии связи {i}'] = service_vars.get(f'lvs_mounting_{i}')
+    #             static_vars[f'Способ крепежа линии связи {i}'] = service_vars.get(f'lvs_fastening_{i}')
+    #             multi_vars[repr_string['mounting_line_service']].append(f'- Смонтировать %Количество линий связи {i}% линии %Тип кабеля {i}% от %Точка от {i}%' +
+    #                                                             f' до %Точка до {i}%. %Способ монтажа линии связи {i}%. %Способ крепежа линии связи {i}%.')
+    #         result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
+    #
+    #     elif tag_service.startswith('phone'):
+    #         template = templates.get('Организация Телефонии')
+    #         static_vars = {}
+    #         hidden_vars = {}
+    #         #repr_string = {}
+    #         multi_vars = {repr_string['mounting_line_service']: []}
+    #         count_lines = [key.strip('phone_from_') for key in service_vars.keys() if key.startswith('phone_from_')]
+    #         for i in count_lines:
+    #             static_vars[f'Количество линий связи {i}'] = service_vars.get(f'phone_count_line_{i}')
+    #             static_vars[f'Тип кабеля {i}'] = service_vars.get(f'phone_cable_{i}')
+    #             static_vars[f'Точка от {i}'] = service_vars.get(f'phone_from_{i}')
+    #             static_vars[f'Точка до {i}'] = service_vars.get(f'phone_to_{i}')
+    #             static_vars[f'Способ монтажа линии связи {i}'] = service_vars.get(f'phone_mounting_{i}')
+    #             static_vars[f'Способ крепежа линии связи {i}'] = service_vars.get(f'phone_fastening_{i}')
+    #             multi_vars[repr_string['mounting_line_service']].append(f'- Смонтировать %Количество линий связи {i}% линии %Тип кабеля {i}% от %Точка от {i}%' +
+    #                                                             f' до %Точка до {i}%. %Способ монтажа линии связи {i}%. %Способ крепежа линии связи {i}%.')
+    #         if service_vars.get('phone_vgw_place') != 'не требуется':
+    #             hidden_vars['Установка оборудования:'] = 'Установка оборудования:'
+    #             hidden_vars['- Установить тел. шлюз %Место голос. шлюза%.'] = '- Установить тел. шлюз %Место голос. шлюза%.'
+    #             static_vars['Место голос. шлюза'] = service_vars.get('phone_vgw_place')
+    #         result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
+    #
+    #     elif tag_service.startswith('video'):
+    #         template = templates.get('Организация СВН')
+    #         static_vars = {}
+    #         hidden_vars = {}
+    #         #repr_string = {}
+    #         multi_vars = {repr_string['mounting_line_service']: []}
+    #         count_lines = [key.strip('video_from_') for key in service_vars.keys() if key.startswith('video_from_')]
+    #         for i in count_lines:
+    #             static_vars[f'Количество линий связи {i}'] = service_vars.get(f'video_count_line_{i}')
+    #             static_vars[f'Тип кабеля {i}'] = service_vars.get(f'video_cable_{i}')
+    #             static_vars[f'Точка от {i}'] = service_vars.get(f'video_from_{i}')
+    #             static_vars[f'Точка до {i}'] = service_vars.get(f'video_to_{i}')
+    #             static_vars[f'Способ монтажа линии связи {i}'] = service_vars.get(f'video_mounting_{i}')
+    #             static_vars[f'Способ крепежа линии связи {i}'] = service_vars.get(f'video_fastening_{i}')
+    #             multi_vars[repr_string['mounting_line_service']].append(f'- Смонтировать %Количество линий связи {i}% линии %Тип кабеля {i}% от %Точка от {i}%' +
+    #                                                             f' до %Точка до {i}%. %Способ монтажа линии связи {i}%. %Способ крепежа линии связи {i}%.')
+    #         if service_vars.get('video_switch'):
+    #             hidden_vars['- По согласованию с клиентом установить POE-коммутатор в помещении клиента.'] = \
+    #                 '- По согласованию с клиентом установить POE-коммутатор в помещении клиента.'
+    #         static_vars['Количество камер'] = service_vars.get('video_count_camera')
+    #         result.append(analyzer_vars(template, static_vars, hidden_vars, multi_vars))
 
     return result
 
@@ -370,50 +375,50 @@ class CopperFormView(CredentialMixin, FormView):
         return context
 
     def get_success_url(self, **kwargs):
-        #return reverse('otpm_data', kwargs={'trID': self.kwargs['trID']})
-        return reverse('otpm_service', kwargs={'trID': self.kwargs['trID']})
-
-
-
-class ServiceFormView(CredentialMixin, FormView):
-    template_name = "oattr/services.html"
-    form_class = ServiceForm
-
-    @cache_check_view
-    def dispatch(self, *args, **kwargs):
-        """Используется для проверки credential"""
-        return super().dispatch(*args, **kwargs)
-
-    @cache_check_view
-    def form_valid(self, form):
-        service_vars = dict(**form.cleaned_data)
-        session_tr_id = self.request.session[str(self.kwargs['trID'])]
-        session_tr_id.update({'service_vars': service_vars})
-        self.request.session[str(self.kwargs['trID'])] = session_tr_id
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        ticket_tr = get_object_or_404(OtpmTR, ticket_tr=self.kwargs['trID'])
-        #services = [service for service in ticket_tr.services if ('Интернет' or 'Хот-спот') not in service]
-        services = {}
-
-
-        tags_services = {'phone': 'Телефон', 'video': 'Видеонаблюдение', 'lvs': 'ЛВС', 'hotspot': 'Хот-спот'}
-        for key, value in tags_services.items():
-            for service in ticket_tr.services:
-                if service.startswith(value):
-                    if services.get(key):
-                        services[key] = services.get(key) + ', ' + service[len(value):].capitalize()
-                    else:
-                        services.update({key: service})
-
-        context['ticket_tr'] = ticket_tr
-        context['services'] = services
-        return context
-
-    def get_success_url(self, **kwargs):
         return reverse('otpm_data', kwargs={'trID': self.kwargs['trID']})
+        #return reverse('otpm_service', kwargs={'trID': self.kwargs['trID']})
+
+
+
+# class ServiceFormView(CredentialMixin, FormView):
+#     template_name = "oattr/services.html"
+#     form_class = ServiceForm
+#
+#     @cache_check_view
+#     def dispatch(self, *args, **kwargs):
+#         """Используется для проверки credential"""
+#         return super().dispatch(*args, **kwargs)
+#
+#     @cache_check_view
+#     def form_valid(self, form):
+#         service_vars = dict(**form.cleaned_data)
+#         session_tr_id = self.request.session[str(self.kwargs['trID'])]
+#         session_tr_id.update({'service_vars': service_vars})
+#         self.request.session[str(self.kwargs['trID'])] = session_tr_id
+#         return super().form_valid(form)
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         ticket_tr = get_object_or_404(OtpmTR, ticket_tr=self.kwargs['trID'])
+#         #services = [service for service in ticket_tr.services if ('Интернет' or 'Хот-спот') not in service]
+#         services = {}
+#
+#
+#         tags_services = {'phone': 'Телефон', 'video': 'Видеонаблюдение', 'lvs': 'ЛВС', 'hotspot': 'Хот-спот'}
+#         for key, value in tags_services.items():
+#             for service in ticket_tr.services:
+#                 if service.startswith(value):
+#                     if services.get(key):
+#                         services[key] = services.get(key) + ', ' + service[len(value):].capitalize()
+#                     else:
+#                         services.update({key: service})
+#
+#         context['ticket_tr'] = ticket_tr
+#         context['services'] = services
+#         return context
+#
+#     def get_success_url(self, **kwargs):
+#         return reverse('otpm_data', kwargs={'trID': self.kwargs['trID']})
 
 
 
